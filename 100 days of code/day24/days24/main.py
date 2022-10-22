d552bd4491dcabcfcc115d4bf99db8f4cb384643
karuhame <62510010+karuhame@users.noreply.github.com>
2022-10-22 10:12:02 +0700

    Add files via upload

---
+++ 100 days of code/day24/days24/main.py
@@ -0,0 +1,9 @@
+# import os
+
+# os.chdir(r'G:\bachkhoa\-python_challenge\100 days of code\days24')
+
+with open('./days24/yaeh.txt','w') as file:
+
+    file.write("son rat dep trai")
+    content = file.read()
+    print(content)
